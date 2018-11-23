ebb2466eacfb01faf7d65f7ab1039ff434f3ab6e
Covee <uniqtop91@gmail.com>
2018-11-23 21:26:48 +0900

    created images app, add it to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,9 +61,9 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
     # 'django.contrib.humanize', # Handy template tags
     'django.contrib.admin',
+
 ]
 THIRD_PARTY_APPS = [
-    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -71,6 +71,8 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'xstagram.users.apps.UsersAppConfig',
+    'xstagram.images.apps.ImagesConfig',
+    
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
