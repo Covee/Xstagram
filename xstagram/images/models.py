ea4c4765b320d53801babaf3f7e2bcabc20ff04f
Covee <uniqtop91@gmail.com>
2018-11-25 20:50:34 +0900

    created Image Model

--- xstagram/images/models.py
+++ xstagram/images/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # (below) 이 클래스가 DB에 저장되지 않게끔 abstract 모델이라고 선언해준다.(모듈처럼 다른 클래스에서 확장하여 쓰려고 만든 모델이기 때문)
+    class Meta:
+        abstract = True 
+
+
+class Image(TimeStampedModel):
+    file        = models.ImageField()
+    location    = models.CharField(max_length=140)
+    caption     = models.TextField()
+
+
+class Comment(TimeStampedModel):
+    message = models.TextField()
+
