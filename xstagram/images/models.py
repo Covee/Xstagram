1ff989c3131b54ed99b5d8997ccfaf894032ce4b
Covee <uniqtop91@gmail.com>
2018-11-26 19:40:28 +0900

    created Like Model & added ForeignKey to every class in images/models.py

--- xstagram/images/models.py
+++ xstagram/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from xstagram.users import models as user_models
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,8 +15,16 @@ class Image(TimeStampedModel):
     file        = models.ImageField()
     location    = models.CharField(max_length=140)
     caption     = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
 
 
 class Comment(TimeStampedModel):
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image   = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
+
+
+class Like(TimeStampedModel):
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image   = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
 
