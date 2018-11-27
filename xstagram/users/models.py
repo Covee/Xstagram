9b84774cec6bd38d44f9651b7bf7591cb3a23691
Covee <uniqtop91@gmail.com>
2018-11-27 18:11:24 +0900

    added following, follower to User Model; using ManyToManyField

--- xstagram/users/models.py
+++ xstagram/users/models.py
@@ -7,18 +7,20 @@ from django.utils.translation import ugettext_lazy as _
 class User(AbstractUser):
 
     # User Model
-    
+
     GENDER_CHOICES = (
         ('male', 'Male'),
         ('female', 'Female'),
         ('not-specified', 'Not specified'),
     )
 
-    name    = models.CharField(_("Name of User"), blank=True, max_length=255)
-    website = models.URLField(null=True)
-    bio     = models.TextField(null=True)
-    phone   = models.CharField(max_length=100, null=True)
-    gender  = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    name        = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website     = models.URLField(null=True)
+    bio         = models.TextField(null=True)
+    phone       = models.CharField(max_length=100, null=True)
+    gender      = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    followers   = models.ManyToManyField("self")
+    following   = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
