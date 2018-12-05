a849f9f5ac03d6d60dae2dc18ab0b27a1103b5d3
Covee <uniqtop91@gmail.com>
2018-12-05 23:21:19 +0900

    added APIviews for all comments and likes

--- xstagram/images/urls.py
+++ xstagram/images/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from .views import (
     ListAllImages,
+    ListAllComments,
+    ListAllLikes,
 
 )
 
 app_name = "images"
 urlpatterns = [
     path("all/", view=ListAllImages.as_view(), name="all_images"),
+    path("comments/", view=ListAllComments.as_view(), name="all_comments"),
+    path("likes/", view=ListAllLikes.as_view(), name="all_likes"),
 ]
