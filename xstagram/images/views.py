7bbedaa2dfbf277eb19b6cb932e32fbe2509017a
Cobee <uniqtop91@gmail.com>
2018-12-17 18:34:41 +0900

    get people to follow & don't need to follow or follwing by anyone, anymore

--- xstagram/images/views.py
+++ xstagram/images/views.py
@@ -1,9 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from . import models
-
-from . import serializers
+from . import models, serializers
 
 
 class Feed(APIView):
