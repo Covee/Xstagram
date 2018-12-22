f98bbf238fa8b4d0c0e6da2a8cf5aaefc6c22743
Cobee <uniqtop91@gmail.com>
2018-12-22 21:46:55 +0900

    follow user

--- xstagram/users/urls.py
+++ xstagram/users/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path("explore", views.ExploreUsers.as_view(), name="explore_users"),
+    path("<int:user_id>/follow", views.FollowUser.as_view(), name="follow_user"),
+    
 ]
